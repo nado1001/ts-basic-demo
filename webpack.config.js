cda4b59ba4d64d68e4403bae73fbe1c6f1e80524
take-tech1001 <tk.tech1001@gmail.com>
2021-05-31 00:32:58 +0900

    Setting webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,32 @@
+const path = require('path')
+
 module.exports = {
   entry: {
     // index.tsをバンドルする
     bundle: './src/index.ts'
+  },
+  output: {
+    // distディレクトリにbundleする
+    path: path.join(__dirname, 'dist'),
+    // bundle.js
+    filename: '[name].js'
+  },
+  resolve: {
+    // import exportの時拡張子を除く
+    extenstions: ['.ts', '.js']
+  },
+  devServer: {
+    // ローカルサーバーが参照するディレクトリ
+    contentBase: path.join(__dirname, 'dist'),
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        // .tsの拡張子をts-loaderでコンパイルする
+        loader: 'ts-loader',
+        test: /\.ts$/
+      }
+    ]
   }
 }
