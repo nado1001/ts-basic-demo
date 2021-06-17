89351f2cf5c14a4547302eb3cd05ec7cdae4f279
take-tech1001 <tk.tech1001@gmail.com>
2021-06-18 01:35:59 +0900

    Add promise type

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,15 @@
 // tupleSample()
 
 // 07. ジェネリック型とポリフォーリズム
-import genericsBasicSample from './generics/basic'
-import genericsAdvancedSample from './generics/advanced'
-genericsBasicSample()
-genericsAdvancedSample()
+// import genericsBasicSample from './generics/basic'
+// import genericsAdvancedSample from './generics/advanced'
+// genericsBasicSample()
+// genericsAdvancedSample()
+
+// 「10.非同期処理の型定義」のサンプルコード
+// import callbackSample from './asynchronous/callback'
+// import promiseSample from './asynchronous/promise'
+import asyncAwaitSample from './asynchronous/asyncAwait'
+// callbackSample()
+// promiseSample()
+asyncAwaitSample()
